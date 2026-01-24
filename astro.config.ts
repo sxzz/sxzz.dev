fc52ef83aa95acd1c0b96d649d24de3c6de3705c
Kevin Deng <sxzz@sxzz.moe>
2026-01-24 10:36:21 +0800

    feat: add cloudflare config

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
-
 import vue from '@astrojs/vue'
 import UnoCSS from '@unocss/astro'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   integrations: [vue(), UnoCSS()],
+  adapter: cloudflare(),
 })
