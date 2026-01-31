d95e97ecc9f17c71b3cfa36d304cfbc8adae37b0
Kevin Deng <sxzz@sxzz.moe>
2026-02-01 04:34:41 +0800

    feat: add GitHub-style admonition rendering and fix inline code overflow

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
 import UnoCSS from '@unocss/astro'
 import cloudflare from '@astrojs/cloudflare'
+import remarkGithubBlockquoteAlert from 'remark-github-blockquote-alert'
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   integrations: [vue(), UnoCSS()],
   adapter: cloudflare(),
+  markdown: {
+    remarkPlugins: [remarkGithubBlockquoteAlert],
+  },
 })
